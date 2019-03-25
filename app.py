c6ada44dd612eca078458f7448ea57e76ace4bcd
Klaus Kirkelund Andersen <klaus1@strateg.dk>
2019-03-25 20:17:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def main():
-    return "Welcome to the New App!"
+    return "Welcome to the New App2!"
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
